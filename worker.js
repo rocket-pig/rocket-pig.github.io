25ec0f1f9f7487e9d19e1cdc59ae4cc033c2876b
rocket-pig <prolefeed@gmail.com>
2023-01-02 22:56:58 -0800

    add cache-first strategy.

--- worker.js
+++ worker.js
@@ -16,5 +16,17 @@ self.addEventListener("activate", event => {
   console.log("Service Worker activating.");
 });
 
-
+self.addEventListener("fetch", event => {
+  // Cache-First Strategy
+  event.respondWith(
+    caches
+      .match(event.request) // check if the request has already been cached
+      .then(cached => cached || fetch(event.request)) // otherwise request network
+      .then(
+        response =>
+          cache(event.request, response) // put response in cache
+            .then(() => response) // resolve promise with the network response
+      )
+  );
+});
 
