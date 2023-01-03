4a7724bfd5061b022b7bc79390d4e2f97cd47dd4
rocket-pig <prolefeed@gmail.com>
2023-01-02 23:47:37 -0800

    .

--- startworker.js
+++ startworker.js
@@ -1,21 +1,12 @@
-// create a worker whose code is defined in the file passed as parameter
-const worker = new Worker("worker.js");
-
-function sendMessageToWorker() {
-  worker.postMessage("hello");
+window.onload = function() {
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("./worker.js")
+    .then(serviceWorker => { serviceWorker.update().then(e=>console.log(e))
+    })
+    .catch(error => {
+      console.error("Error registering and updating the Service Worker: ", error);
+    });
 }
 
-function askWorkerRecurringTask() {
-  worker.postMessage("recurring");
-}
-
-// This event is fired when the worker posts a message
-// The value of the message is in messageEvent.data
-worker.addEventListener("message", function(messageEvent) {
-  // Log the received message in the output element
-  //const log = document.createElement("p");
-  //log.textContent = messageEvent.data;
-  //document.querySelector("output").prepend(log);
-  console.log(messageEvent)
-});
-
+} //onload
