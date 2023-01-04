5dca8684810e411acfb3d5988b04f88c1d72eb4c
rocket-pig <prolefeed@gmail.com>
2023-01-03 22:02:14 -0800

    wow finally a ping between worker and client that works.

--- worker.js
+++ worker.js
@@ -2,26 +2,21 @@
 
 const CACHE_NAME = "V1"
 
-var log=[];
-z = console.log
-console.log = function(msg,z) {
-  log.push(msg);
-  z(msg);
-}
-
-
-//make a pipeline out of special context for log messages. Doesnt work as far as I can tell.  For a brief moment the 'window.sw' assignment worked and I could toy with it. Unclear why it stopped. 
-self.addEventListener('message', messageEvent => {
-  if (messageEvent.data === 'skipWaiting') return skipWaiting();
-  if (messageEvent.data === 'getLog') return log;
-  
-});
+//https://developer.mozilla.org/en-US/docs/Web/API/Worker/postMessage
+//https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerGlobalScope/message_event
+//ive come to expect examples to be wrong. finally figured out a way that works:
+self.onmessage = function handleMsgfromMain(messageEvent) {
+    if (messageEvent.data === 'ping'){ 
+    console.log('[Client]: '+messageEvent.data);
+    messageEvent.source.postMessage('pong'); 
+        }
+    }; 
 
 //install 
 //InstallEvent is apparently 'deprecated'
 //https://developer.mozilla.org/en-US/docs/Web/API/InstallEvent
 self.addEventListener("install", event => {
-    console.log('[Service Worker] Inside deprecated install event...unclear what it even does...')
+    console.log('[Service Worker] "Installing"...')
       self.skipWaiting();
  caches.open(CACHE_NAME).then(cache=>console.log('[Service Worker] Cache '+CACHE_NAME+' opened.'))
 });
@@ -32,7 +27,7 @@ self.addEventListener("activate", event => {
   console.log("[Service Worker] Activating...");
 });
 
-//fetch plan. This caches _everything and may be the source of the extreme minute-long start times? Still dont know.
+//fetch plan. This caches _everything and may be the source of the intermittent, extreme minute-long start times? Still dont know.
 self.addEventListener("fetch", (e) => {
   e.respondWith(
     (async () => {
